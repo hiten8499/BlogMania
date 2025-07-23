6c0dc6a4f59e48bc87fdfeafe935deeb49b7da32
hiten8499 <purohithiten49@gmail.com>
2025-07-23 23:10:23 +0530

    Completed the AdminPanel with static data (FRONTEND )by adding content to the remaining pages

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
-import { assets } from '../../assets/assets'
-import { Outlet, useNavigate } from 'react-router-dom'
+import React from 'react';
+import { assets } from '../../assets/assets';
+import { Outlet, useNavigate } from 'react-router-dom';
 import SideBar from '../../components/admin/SideBar';
 
-
 const Layout = () => {
+  const navigate = useNavigate();
 
-    const navigate=useNavigate()
+  const logout = () => {
+    navigate('/');
+  };
 
-    const logout=()=>{
-        navigate('/')
-    }
   return (
     <>
-      <div className='flex items-center justify-between py-2-h-[70px] px-4 sm:px-12
-      border-b border-gray-200 mt-3'>
-         <img src={assets.logo}  className='w-32 sm:w-40 cursor-pointer' alt="" 
-         onClick={()=>navigate('/')}/>
-         <button type="submit" className='text-sm px-8 py-2 bg-primary text-white 
-          rounded-full cursor-pointer ' onClick={()=>logout}>Logout</button>
+      <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200 mt-3'>
+        <img
+          src={assets.logo}
+          className='w-32 sm:w-40 cursor-pointer'
+          alt='Logo'
+          onClick={() => navigate('/')}
+        />
+        <button
+          type='button'
+          className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'
+          onClick={logout}
+        >
+          Logout
+        </button>
       </div>
 
       <div className='flex h-[calc(100vh-70px)]'>
-            <SideBar/>
-            <Outlet/>
-
+        <SideBar />
+        <Outlet />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
