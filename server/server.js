6674f087a4749ad96fa7b8f84d5bedc49987f210
hiten8499 <purohithiten49@gmail.com>
2025-07-24 20:04:25 +0530

    Add Node/Express backend server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './configs/db.js';
+
+const app=express();
+
+await connectDB()
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/',(req,res)=> res.send("Api is Working.."))
+
+const PORT= process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+console.log('Server Running on thr Port '+PORT)
+})
+export default app;
